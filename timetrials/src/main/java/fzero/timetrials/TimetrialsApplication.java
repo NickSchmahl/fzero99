24a27a3f34c0d580c9f65bbac239fed8dff4b618
Nick Schmahl <schmahlnick@gmail.com>
2023-10-04 13:53:33 +0200

    Initial commit

---
+++ timetrials/src/main/java/fzero/timetrials/TimetrialsApplication.java
@@ -0,0 +1,13 @@
+package fzero.timetrials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetrialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimetrialsApplication.class, args);
+	}
+
+}
---
+++ timetrials/src/test/java/fzero/timetrials/TimetrialsApplicationTests.java
@@ -0,0 +1,13 @@
+package fzero.timetrials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetrialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
