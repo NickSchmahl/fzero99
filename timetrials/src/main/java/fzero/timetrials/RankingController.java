df4b392a0aaed6cfb4c86abaff63095cda885039
Nick Schmahl <schmahlnick@gmail.com>
2023-10-04 19:15:19 +0200

    First site done

---
+++ timetrials/src/main/java/fzero/timetrials/RankingController.java
@@ -0,0 +1,14 @@
+package fzero.timetrials;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("rankings")
+public class RankingController {
+    @GetMapping("")
+    public String getRankings() {
+        return "ranking";
+    }
+}
