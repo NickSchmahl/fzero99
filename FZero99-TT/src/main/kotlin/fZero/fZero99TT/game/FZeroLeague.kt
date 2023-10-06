02c27a74cc12b2dc0ea0a256c6d0d5e8fe72cdba
Nick Schmahl <schmahlnick@gmail.com>
2023-10-06 23:39:51 +0200

    Enums for Leagues and Tracks

---
+++ FZero99-TT/src/main/kotlin/fZero/fZero99TT/game/FZeroLeague.kt
@@ -0,0 +1,7 @@
+package fZero.fZero99TT.game
+
+enum class FZeroLeague(val longName: String) {
+    KNIGHT("Knight League"),
+    QUEEN("Queen League"),
+    KING("King League")
+}
---
+++ FZero99-TT/src/main/kotlin/fZero/fZero99TT/game/FZeroTrack.kt
@@ -0,0 +1,14 @@
+package fZero.fZero99TT.game
+
+enum class FZeroTrack(val realName: String, val league: FZeroLeague) {
+    MUTE_CITY_1("Mute City 1", FZeroLeague.KNIGHT),
+    MUTE_CITY_2("Mute City 2", FZeroLeague.QUEEN),
+    DEATH_WIND_1("Death Wind 1", FZeroLeague.KNIGHT),
+    DEATH_WIND_2("Death Wind 2", FZeroLeague.KING),
+    PORT_TOWN_1("Port Town 1", FZeroLeague.QUEEN),
+    PORT_TOWN_2("Port Town 2", FZeroLeague.KING),
+    WHITE_LAND_1("White Land 1", FZeroLeague.QUEEN),
+    SAND_OCEAN("Sand Ocean", FZeroLeague.KNIGHT),
+    BIG_BLUE("Big Blue", FZeroLeague.KNIGHT),
+    SILENCE("Silence", FZeroLeague.KNIGHT),
+}
