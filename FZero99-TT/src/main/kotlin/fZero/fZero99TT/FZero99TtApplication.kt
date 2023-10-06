209cfc635019b0b24b329da4b594bf078ae90f85
Nick Schmahl <schmahlnick@gmail.com>
2023-10-06 21:36:14 +0200

    Kootlin

---
+++ FZero99-TT/src/main/kotlin/fZero/fZero99TT/FZero99TtApplication.kt
@@ -0,0 +1,11 @@
+package fZero.fZero99TT
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FZero99TtApplication
+
+fun main(args: Array<String>) {
+	runApplication<FZero99TtApplication>(*args)
+}
---
+++ FZero99-TT/src/test/kotlin/fZero/fZero99TT/FZero99TtApplicationTests.kt
@@ -0,0 +1,13 @@
+package fZero.fZero99TT
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FZero99TtApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
