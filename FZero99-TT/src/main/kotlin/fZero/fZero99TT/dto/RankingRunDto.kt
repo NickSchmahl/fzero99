ce873c59b51679406e3b8455c632daf0815eaf57
Nick Schmahl <schmahlnick@gmail.com>
2023-10-15 18:29:00 +0200

    time representation

--- FZero99-TT/src/main/kotlin/fZero/fZero99TT/dto/RankingRunDto.kt
+++ FZero99-TT/src/main/kotlin/fZero/fZero99TT/dto/RankingRunDto.kt
@@ -2,6 +2,7 @@ package fZero.fZero99TT.dto
 
 import fZero.fZero99TT.game.FZeroGlider
 import fZero.fZero99TT.game.FZeroTrack
+import kotlin.time.Duration.Companion.seconds
 
 data class RankingRunDto(
     val track: FZeroTrack,
@@ -12,4 +13,13 @@ data class RankingRunDto(
     val round3: Double,
     val round4: Double,
 ) {
+    val totalTimeString = this.totalTime.toDurationString()
+    val round1String = this.round1.toDurationString()
+    val round2String = this.round2.toDurationString()
+    val round3String = this.round3.toDurationString()
+    val round4String = this.round4.toDurationString()
+}
+
+fun Double.toDurationString(): String {
+    return this.seconds.toString().replace(".", ",")
 }
